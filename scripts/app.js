fd7f7984d277fe3a958799a05e10e8f1e46b4e57
fadelanumah <fadelanumah@gmail.com>
2024-10-18 11:35:10 +0700

    feat(category-detail): Add Meals Data

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+  function MealCategoryCard(mealCategory) {
+    return `
+            <div class="card col-4" style="width: 18rem;">
+              <img src="${mealCategory.strCategoryThumb}" class="card-img-top p-3" alt="...">
+              <div class="card-body text-center d-flex flex-column justify-content-between gap-3">
+              <h5 class="card-title">${mealCategory.strCategory}</h5>
+              <div>
+                  <a href="#" class="btn btn-dark meals-category-link" data-name="${mealCategory.strCategory}">Check Meals</a>
+              </div>
+            </div>
+            </div>
+        `
+  }
+
   let mealCategories = []
 
   $.get('https://www.themealdb.com/api/json/v1/1/categories.php', function (data) {
@@ -8,18 +22,6 @@ $(document).ready(function () {
 
     $('#mealCategoriesContainer').html(cards)
   })
-
-  function MealCategoryCard(mealCategory) {
-    return `
-            <div class="card col-4" style="width: 18rem;">
-              <img src="${mealCategory.strCategoryThumb}" class="card-img-top p-3" alt="...">
-              <div class="card-body text-center">
-                <h5 class="card-title">${mealCategory.strCategory}</h5>
-                <a href="#" class="btn btn-dark meals-category-link" data-name="${mealCategory.strCategory}">Check Meals</a>
-              </div>
-            </div>
-        `
-  }
 
   // Get meals by category
   $(document).on('click', '.meals-category-link', function (e) {
--- scripts/getDetailCategory.js
+++ scripts/getDetailCategory.js
@@ -1,5 +1,46 @@
 $(document).ready(function () {
+    function MealCard(meal) {
+        return `
+                <div class="card col-4 p-3" style="width: 18rem;">
+                  <img src="${meal.strMealThumb}" class="card-img-top" alt="..." style="height: 262px;">
+                  <div class="card-body text-center d-flex flex-column justify-content-between gap-3">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <div>
+                        <a href="#" class="btn btn-dark meals-category-link" data-name="${meal.idMeal}">Check Recipe</a>
+                    </div>
+                  </div>
+                </div>
+            `
+      }
+
     const urlParams = new URLSearchParams(window.location.search)
+    
+    const selectedCategoryData = JSON.parse(localStorage.getItem('selectedCategoryData'));
+
+    $('#categoryThumb').attr('src', selectedCategoryData.strCategoryThumb).attr('alt', `${selectedCategoryData.strCategory} meals image`)
+    $('.category-name').html(selectedCategoryData.strCategory)
+    $('#categoryDescription').html(selectedCategoryData.strCategoryDescription)
 
     const categoryName = urlParams.get('categoryName')
+    
+    const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+
+    let meals = []
+
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        success: function (data) {
+            meals = data.meals
+
+            const cards = meals.map((meal) => MealCard(meal)).join('')
+
+            $('#categoryMealsContainer').html(cards)
+
+        },
+        error: function () {
+            console.log('error get meals item');
+        }
+
+    })
 })
